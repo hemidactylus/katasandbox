af54dcc5797d6e094ca4e26077e15e19275eb742
Stefano Lottini <stefano.lottini@datastax.com>
2021-08-23 15:16:04 +0200

    two-node-cluster test1

--- repair-improvements/background.sh
+++ repair-improvements/background.sh
@@ -6,14 +6,14 @@ wget https://archive.apache.org/dist/cassandra/4.0-beta2/apache-cassandra-4.0-be
 
 ssh root@[[HOST1_IP]] -o StrictHostKeyChecking=no "tar xzf apache-cassandra-4.0-beta2-bin.tar.gz"
 ssh root@[[HOST2_IP]] -o StrictHostKeyChecking=no "tar xzf apache-cassandra-4.0-beta2-bin.tar.gz"
-ssh root@[[HOST1_IP]] "sed -i 's/^cluster_name: .*$/cluster_name: \"Cassandra Cluster\"/g' apache-cassandra-4.0-beta2/conf/cassandra.yaml"
-ssh root@[[HOST2_IP]] "sed -i 's/^cluster_name: .*$/cluster_name: \"Cassandra Cluster\"/g' apache-cassandra-4.0-beta2/conf/cassandra.yaml"
+# ssh root@[[HOST1_IP]] "sed -i 's/^cluster_name: .*$/cluster_name: \"Cassandra Cluster\"/g' apache-cassandra-4.0-beta2/conf/cassandra.yaml"
+# ssh root@[[HOST2_IP]] "sed -i 's/^cluster_name: .*$/cluster_name: \"Cassandra Cluster\"/g' apache-cassandra-4.0-beta2/conf/cassandra.yaml"
 
-ssh root@[[HOST1_IP]] "sed -i 's/^endpoint_snitch: .*$/endpoint_snitch: GossipingPropertyFileSnitch/g' apache-cassandra-4.0-beta2/conf/cassandra.yaml"
-ssh root@[[HOST2_IP]] "sed -i 's/^endpoint_snitch: .*$/endpoint_snitch: GossipingPropertyFileSnitch/g' apache-cassandra-4.0-beta2/conf/cassandra.yaml"
+# ssh root@[[HOST1_IP]] "sed -i 's/^endpoint_snitch: .*$/endpoint_snitch: GossipingPropertyFileSnitch/g' apache-cassandra-4.0-beta2/conf/cassandra.yaml"
+# ssh root@[[HOST2_IP]] "sed -i 's/^endpoint_snitch: .*$/endpoint_snitch: GossipingPropertyFileSnitch/g' apache-cassandra-4.0-beta2/conf/cassandra.yaml"
 
-ssh root@[[HOST1_IP]] "sed -i 's/^dc=.*$/dc=DC-West/g' apache-cassandra-4.0-beta2/conf/cassandra-rackdc.properties"
-ssh root@[[HOST2_IP]] "sed -i 's/^dc=.*$/dc=DC-East/g' apache-cassandra-4.0-beta2/conf/cassandra-rackdc.properties"
+# ssh root@[[HOST1_IP]] "sed -i 's/^dc=.*$/dc=DC-West/g' apache-cassandra-4.0-beta2/conf/cassandra-rackdc.properties"
+# ssh root@[[HOST2_IP]] "sed -i 's/^dc=.*$/dc=DC-East/g' apache-cassandra-4.0-beta2/conf/cassandra-rackdc.properties"
 
 ssh root@[[HOST1_IP]] "sed -i 's/^listen_address:.*$/listen_address: [[HOST1_IP]]/g' apache-cassandra-4.0-beta2/conf/cassandra.yaml"
 ssh root@[[HOST2_IP]] "sed -i 's/^listen_address:.*$/listen_address: [[HOST2_IP]]/g' apache-cassandra-4.0-beta2/conf/cassandra.yaml"
@@ -22,8 +22,8 @@ ssh root@[[HOST1_IP]] "sed -i 's/^rpc_address:.*$/rpc_address: [[HOST1_IP]]/g' a
 ssh root@[[HOST2_IP]] "sed -i 's/^rpc_address:.*$/rpc_address: [[HOST2_IP]]/g' apache-cassandra-4.0-beta2/conf/cassandra.yaml"
 
 #seeds:
-ssh root@[[HOST1_IP]] "sed -i 's/127.0.0.1/[[HOST1_IP]]/g' apache-cassandra-4.0-beta2/conf/cassandra.yaml"
-ssh root@[[HOST2_IP]] "sed -i 's/127.0.0.1/[[HOST1_IP]]/g' apache-cassandra-4.0-beta2/conf/cassandra.yaml"
+# ssh root@[[HOST1_IP]] "sed -i 's/127.0.0.1/[[HOST1_IP]]/g' apache-cassandra-4.0-beta2/conf/cassandra.yaml"
+# ssh root@[[HOST2_IP]] "sed -i 's/127.0.0.1/[[HOST1_IP]]/g' apache-cassandra-4.0-beta2/conf/cassandra.yaml"
    
 ssh root@[[HOST1_IP]] "mv apache-cassandra-4.0-beta2 /usr/share/cassandra"
 ssh root@[[HOST2_IP]] "mv apache-cassandra-4.0-beta2 /usr/share/cassandra"
@@ -45,7 +45,8 @@ echo '[[HOST2_IP]] node2' >> /etc/hosts
 ssh root@[[HOST1_IP]] '/usr/share/cassandra/bin/cassandra -R'
 ssh root@[[HOST1_IP]] 'while [ `grep "Starting listening for CQL clients" /usr/share/cassandra/logs/system.log | wc -l` -lt 1 ]; do sleep 10; done'
 
-ssh root@[[HOST2_IP]] '/usr/share/cassandra/bin/cassandra -R'
+sleep 20
+ssh root@[[HOST2_IP]] ' CASSANDRA_SEEDS=[[HOST1_IP]] /usr/share/cassandra/bin/cassandra -R'
 ssh root@[[HOST2_IP]] 'while [ `grep "Starting listening for CQL clients" /usr/share/cassandra/logs/system.log | wc -l` -lt 1 ]; do sleep 10; done'
 
 ssh root@[[HOST1_IP]] "echo \"done\" >> /opt/katacoda-background-finished"
